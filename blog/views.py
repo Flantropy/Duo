4abd8f4c1f13276a4f37441b9c40d5d428218793
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-18 00:06:09 +0300

    Added base template add Bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'nikita',
+        'title': 'post 1',
+        'content': 'content of post 1'
+    },
+    {
+        'author': 'nelly',
+        'title': 'post 2',
+        'content': 'hello world'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Welcome'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})

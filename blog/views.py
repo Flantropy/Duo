9558c405c516342832b7ca2cab42190fa2c9b247
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-18 17:36:46 +0300

    Added Post model for blog

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'nikita',
-        'title': 'post 1',
-        'content': 'I love her',
-        'date_posted': "May 18, 2020"
-    },
-    {
-        'author': 'nelly',
-        'title': 'post 2',
-        'content': 'I love him',
-        'date_posted': "May 18, 2020"
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Welcome'
     }
     return render(request, 'blog/home.html', context)

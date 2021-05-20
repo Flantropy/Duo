2fe90f1066359851bbf45f41899d77fa0547f68c
Flantropy <fantropy@gmail.com>
2021-05-20 22:40:37 +0300

    Create and Detail forms added for Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from . import views
 from django.urls import path
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView
+)
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]

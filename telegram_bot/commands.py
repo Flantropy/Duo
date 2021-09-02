0b3c36f7e493fba36b9b8e291ee315abd5b137bf
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-09-03 00:58:17 +0300

    base url error fixed 2

--- telegram_bot/commands.py
+++ telegram_bot/commands.py
@@ -10,7 +10,7 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def get_all_posts(update: Update, context: CallbackContext) -> None:
-    BASE_URL = 'http://127.0.0.1:8000/' if (getenv('IS_IN_DEBUG') == 'True') else 'https://frootpy.herokuapp.com'
+    BASE_URL = 'http://127.0.0.1:8000/' if (getenv('IS_IN_DEBUG') == 'True') else 'https://frootpy.herokuapp.com/'
     response = urllib.request.urlopen(BASE_URL + 'api/posts/').read()
     posts = json.loads(response)
     message = ''

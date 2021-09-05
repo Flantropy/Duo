fccd097416e16776b1a9c5c41d7afdc60b42e7fc
Flantropy <80071496+flantropy@users.noreply.github.com>
2021-09-05 07:58:00 +0300

    In message with list of posts - title is hyperlink to web page with that post

--- telegram_bot/commands.py
+++ telegram_bot/commands.py
@@ -1,8 +1,10 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from os import getenv
 import json
 import urllib.request
+from os import getenv
+
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.constants import PARSEMODE_HTML
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -15,5 +17,11 @@ def get_all_posts(update: Update, context: CallbackContext) -> None:
     posts = json.loads(response)
     message = ''
     for post in posts:
-        message += f'{post["id"]}. {post["title"]}\n'
-    update.message.reply_text(message)
+        post_url = f'{BASE_URL}post/{post["id"]}/'
+        message += f'#{post["id"]}. <a href="{post_url}">{post["title"]}</a>\n'
+    update.message.reply_text(message, parse_mode=PARSEMODE_HTML)
+
+
+def get_post_by_id(update: Update, context: CallbackContext) -> None:
+    # TODO: add endpoint in api.views for getting specified post by it's id
+    pass
